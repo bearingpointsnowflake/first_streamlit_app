df5520e048150d56f783d39a7243fc8ceb404f81
bearingpointsnowflake <140511479+bearingpointsnowflake@users.noreply.github.com>
2023-07-25 14:25:02 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,6 @@
 import streamlit 
 streamlit.title('Wtf do you want?')
+streamlit.header('Really tho, what do you want?!')
+streamlit.text('Eat chicken?')
+streamlit.text('Eat pork??')
+streamlit.text('Eat hamburger?!?')
